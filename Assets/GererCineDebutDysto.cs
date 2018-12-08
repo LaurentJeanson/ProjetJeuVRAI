3738da110f7b38d1650039ff075632aa8eede44c
Laurent <jeanson_laurent@outlook.com>
2018-12-08 15:15:44 -0500

    CinematiqueLaurentFinis

--- Assets/GererCineDebutDysto.cs
+++ Assets/GererCineDebutDysto.cs
@@ -8,6 +8,12 @@ public class GererCineDebutDysto : MonoBehaviour {
     public GameObject Camera2;
     public GameObject Personnage2;
     public GameObject Camera3;
+	public GameObject EnnemiStable;
+	public GameObject EnnemiBouge;
+	public GameObject Camera5;
+	public GameObject EnnemiEtAnimal;
+	public GameObject animal;
+	public GameObject GroupeEnnemis;
     // Use this for initialization
     void Start () {
 		
@@ -26,13 +32,25 @@ public class GererCineDebutDysto : MonoBehaviour {
         Personnage1.SetActive(false);
         Camera2.SetActive(true);
         Personnage2.SetActive(true);
+		Destroy (Camera1);
         yield return new WaitForSeconds(4);
         print(Time.time);
         Camera2.SetActive(false);
         Camera3.SetActive(true);
+		Destroy (Camera2);
         yield return new WaitForSeconds(4);
         print(Time.time);
         Camera3.SetActive(false);
         Personnage2.SetActive(false);
+		EnnemiStable.SetActive(false);
+		EnnemiBouge.SetActive(true);
+		Destroy (Camera3);
+		yield return new WaitForSeconds(4);
+		print(Time.time);
+		EnnemiBouge.SetActive(false);
+		Camera5.SetActive(true);
+		animal.SetActive(false);
+		EnnemiEtAnimal.SetActive(true);
+		GroupeEnnemis.SetActive(true);
     }
 }
