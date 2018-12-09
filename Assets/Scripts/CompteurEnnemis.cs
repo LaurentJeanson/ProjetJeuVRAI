e9a8e55f23d766242b9ea131d108117e5b63702e
Carolanne <myridya@hotmail.com>
2018-12-09 03:50:09 -0500

    Modification du script CompteurEnnemis
    Modification pour le script ennemis, le décompte peut se faire manuellement pour le moment, il faut l'ajuster pour qu'il se fais automatiquement tout au long du jeu.

--- Assets/Scripts/CompteurEnnemis.cs
+++ Assets/Scripts/CompteurEnnemis.cs
@@ -12,25 +12,22 @@ using UnityEngine.UI;
 public class CompteurEnnemis : MonoBehaviour {
 
     public int nbEnnemis = 0; // Le nombre d'ennemis au départ
-    private Text Compteur; // Montrer combien d'ennemis il reste
+    private Text txtCompteur; // Montrer combien d'ennemis il reste
+    private GameObject[] EnnTab; // Tableau permettant de calculer le nombre d'ennemis
+    public GameObject GenerateurEnnemis; // Permet d'accéder au script lui étant relié pouvant ainsi utilisre les variable déclarées dans ce script
     
     void Start () {
 
         // Définir le compteur
-        Compteur = GameObject.Find("Compteur").GetComponent<Text>();
-
-        /*Compte ennemis présent dans la scène*/
-        nbEnnemis = GameObject.FindGameObjectsWithTag("ennemi").Length;
-        
-        // Affiche le décompte des ennemis restants dans l'écran de jeu
-        Compteur.text = "Ennemis : " + nbEnnemis;
+        txtCompteur = GameObject.Find("CompteurEnnemis").GetComponent<Text>();
 
 	}
 
   
     void Update () {
 		
-
+        // Affiche le décompte des ennemis restants dans l'écran de jeu
+        txtCompteur.text = "Ennemis restants: " + (GenerateurEnnemis.GetComponent<GenerationEnnemis>().nbEnnemisMax - GenerateurEnnemis.GetComponent<GenerationEnnemis>().iNbEnnemisMorts);
 
 
 
