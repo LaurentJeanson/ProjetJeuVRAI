24a82fc5d3e298945b67240884defd13a7f3b9e6
Phil <philippe.thibeault99@outlook.com>
2018-12-20 12:14:13 -0500

    DEFH:UIOEFGG

--- Assets/Scripts/ChangementSceneMenu.cs
+++ Assets/Scripts/ChangementSceneMenu.cs
@@ -45,4 +45,9 @@ public class ChangementSceneMenu : MonoBehaviour {
     {
         SceneManager.LoadScene("SceneCinematiqueDebut");
     }
+
+    public void AllerCredit()
+    {
+        SceneManager.LoadScene("SceneCredit");
+    }
 }
--- Assets/Scripts/ControlEcranCredit.cs
+++ Assets/Scripts/ControlEcranCredit.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ControlEcranCredit : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        StartCoroutine("RetourMenu");
 	}
 	
 	// Update is called once per frame
 	void update() {
 		
 	}
+
+    IEnumerator RetourMenu()
+    {
+        yield return new WaitForSeconds(26);
+        SceneManager.LoadScene("menuPrincipal");
+    }
 }
