ff83be6662f961e1bd08b3964900420582380b29
Laurent <jeanson_laurent@outlook.com>
2018-12-21 03:01:10 -0500

    Yo

--- Assets/Scripts/CamScriptHopitDysto.cs
+++ Assets/Scripts/CamScriptHopitDysto.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Script créé par Laurent Jeanson. Ce script gère la cinématique lorsque le joueur entre dans l'hopital
+
 public class CamScriptHopitDysto : MonoBehaviour {
 	public GameObject CameraEvent;
 	public GameObject CameraPerso;
@@ -23,12 +25,18 @@ public class CamScriptHopitDysto : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//Lorsque la scène est chargé, la fonction CineEventHopitalDysto commence.
 		StartCoroutine(CineEventHopitalDysto ());
 	}
+
+	//Fonction qui contrôle le texte, panel et caméra selon un temps donné
+
 	IEnumerator CineEventHopitalDysto(){
+		//Après les premières 5 secondes, le texte 1 disparait
         yield return new WaitForSeconds(5);
         textePerso.SetActive(false);
         panelTextPerso.SetActive(false);
+		//Les 15 secondes suivantes, la caméra de la cinématique se désactive et les UI du jeu apparaissent.
         yield return new WaitForSeconds (15);
 		print (Time.time);
 		CameraEvent.SetActive (false);
